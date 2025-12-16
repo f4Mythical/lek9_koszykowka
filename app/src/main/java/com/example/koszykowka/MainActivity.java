0bc2c9eef71cd94d0b615f7f05ed17b30de70ff7
f4Mythical <grynka.adam@gmail.com>
2025-12-16 09:35:10 +0100

    dziala wszystko

--- app/src/main/java/com/example/koszykowka/MainActivity.java
+++ app/src/main/java/com/example/koszykowka/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.koszykowka;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,8 +16,9 @@ import com.example.koszykowka.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding; // klasa z bindowaymi widokami dla pliku activity main xml jest activitymain binding
-    PunktyViewModel punktyViewModel ;
+    private ActivityMainBinding binding;
+    PunktyViewModel punktyViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,44 +28,143 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        punktyViewModel = new ViewModelProvider(this)
-                .get(PunktyViewModel.class);
-        // korzystne by zapisywac pliki przy obracaniu
-      punktyViewModel.getPunkty().observe(this,
-              new Observer<Integer>() {
-                  @Override
-                  public void onChanged(Integer integer) {
-                      binding.textViewLicznik.setText(""+integer);
-                  }
-              }
-      );
-        binding.buttonPlus1.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        punktyViewModel.addPunkty(1);
-                    }
-                });
-                binding.buttonPlus2.setOnClickListener(
-                        new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-punktyViewModel.addPunkty(2);
-                            }
-                        });
-        binding.buttonPlus3.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-punktyViewModel.addPunkty(3);
-                    }
-                });
 
+        punktyViewModel = new ViewModelProvider(this).get(PunktyViewModel.class);
 
+        punktyViewModel.getPunktyGracz1().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer value) {
+                Integer v = value;
+                if (v == null) {
+                    v = 0;
+                }
+                if (!binding.switchGracze.isChecked()) {
+                    binding.textViewLicznik.setText(String.valueOf(v));
+                }
+                int p1 = v;
+                Integer tmp2 = punktyViewModel.getPunktyGracz2().getValue();
+                int p2;
+                if (tmp2 == null) {
+                    p2 = 0;
+                } else {
+                    p2 = tmp2;
+                }
+                binding.textViewWynik.setText("Gracz1: " + p1 + "  Gracz2: " + p2);
+            }
+        });
+
+        punktyViewModel.getPunktyGracz2().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer value) {
+                Integer v = value;
+                if (v == null) {
+                    v = 0;
+                }
+                if (binding.switchGracze.isChecked()) {
+                    binding.textViewLicznik.setText(String.valueOf(v));
+                }
+                int p2 = v;
+                Integer tmp1 = punktyViewModel.getPunktyGracz1().getValue();
+                int p1;
+                if (tmp1 == null) {
+                    p1 = 0;
+                } else {
+                    p1 = tmp1;
+                }
+                binding.textViewWynik.setText("Gracz1: " + p1 + "  Gracz2: " + p2);
+            }
+        });
+
+        binding.buttonPlus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.switchGracze.isChecked()) {
+                    punktyViewModel.addPunktyGracza(2, 1);
+                } else {
+                    punktyViewModel.addPunktyGracza(1, 1);
+                }
+            }
+        });
+
+        binding.buttonPlus2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.switchGracze.isChecked()) {
+                    punktyViewModel.addPunktyGracza(2, 2);
+                } else {
+                    punktyViewModel.addPunktyGracza(1, 2);
+                }
+            }
+        });
+
+        binding.buttonPlus3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.switchGracze.isChecked()) {
+                    punktyViewModel.addPunktyGracza(2, 3);
+                } else {
+                    punktyViewModel.addPunktyGracza(1, 3);
+                }
+            }
+        });
+
+        binding.switchGracze.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    binding.switchGracze.setText("Gracz 2");
+                } else {
+                    binding.switchGracze.setText("Gracz 1");
+                }
+                Integer val;
+                if (isChecked) {
+                    val = punktyViewModel.getPunktyGracz2().getValue();
+                } else {
+                    val = punktyViewModel.getPunktyGracz1().getValue();
+                }
+                if (val == null) {
+                    val = 0;
+                }
+                binding.textViewLicznik.setText(String.valueOf(val));
+                Integer tmp1 = punktyViewModel.getPunktyGracz1().getValue();
+                int p1;
+                if (tmp1 == null) {
+                    p1 = 0;
+                } else {
+                    p1 = tmp1;
+                }
+                Integer tmp2 = punktyViewModel.getPunktyGracz2().getValue();
+                int p2;
+                if (tmp2 == null) {
+                    p2 = 0;
+                } else {
+                    p2 = tmp2;
+                }
+                binding.textViewWynik.setText("Gracz1: " + p1 + "  Gracz2: " + p2);
+            }
+        });
+
+        binding.switchGracze.setChecked(false);
+
+        Integer tmp1 = punktyViewModel.getPunktyGracz1().getValue();
+        int p1;
+        if (tmp1 == null) {
+            p1 = 0;
+        } else {
+            p1 = tmp1;
+        }
+        Integer tmp2 = punktyViewModel.getPunktyGracz2().getValue();
+        int p2;
+        if (tmp2 == null) {
+            p2 = 0;
+        } else {
+            p2 = tmp2;
+        }
+        binding.textViewWynik.setText("Gracz1: " + p1 + "  Gracz2: " + p2);
     }
 }
--- app/src/main/java/com/example/koszykowka/PunktyViewModel.java
+++ app/src/main/java/com/example/koszykowka/PunktyViewModel.java
@@ -3,34 +3,41 @@ package com.example.koszykowka;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import java.util.Random;
-
 public class PunktyViewModel extends ViewModel {
-    private MutableLiveData<Integer>punkty;
+    private MutableLiveData<Integer> punktyGracz1;
+    private MutableLiveData<Integer> punktyGracz2;
 
-    public MutableLiveData<Integer> getPunkty() {
-        if (punkty == null){
-            punkty = new MutableLiveData<>();
-            punkty.setValue(0);
-
+    public MutableLiveData<Integer> getPunktyGracz1() {
+        if (punktyGracz1 == null) {
+            punktyGracz1 = new MutableLiveData<>();
+            punktyGracz1.setValue(0);
         }
-        return punkty;
+        return punktyGracz1;
     }
 
-    public void setPunkty(MutableLiveData<Integer> punkty) {
-        if (punkty == null){
-            punkty = new MutableLiveData<>();
+    public MutableLiveData<Integer> getPunktyGracz2() {
+        if (punktyGracz2 == null) {
+            punktyGracz2 = new MutableLiveData<>();
+            punktyGracz2.setValue(0);
         }
-        this.punkty = punkty;
+        return punktyGracz2;
     }
 
-    public void addPunkty(int p){
-        if(punkty== null){
-            punkty = new MutableLiveData<>();
-        punkty.setValue(0);
+    public void addPunktyGracza(int gracz, int p) {
+        if (gracz == 1) {
+            MutableLiveData<Integer> ld = getPunktyGracz1();
+            Integer curr = ld.getValue();
+            if (curr == null) {
+                curr = 0;
+            }
+            ld.setValue(curr + p);
+        } else {
+            MutableLiveData<Integer> ld = getPunktyGracz2();
+            Integer curr = ld.getValue();
+            if (curr == null) {
+                curr = 0;
+            }
+            ld.setValue(curr + p);
         }
-        int aktualnePunkty =punkty.getValue();
-        punkty.setValue(aktualnePunkty+p);
-
     }
 }
